6f62fa98f06d22cba40064f531babceeba101232
Barış Türe <baristure@hotmail.com>
2020-03-25 23:31:07 +0300

    Updates

---
+++ precache-manifest.80b55c7ce9f47e00c848103f92c01ef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e38217aca1529f3c863adb7c1bc735",
+    "url": "/robotic-rolodex/index.html"
+  },
+  {
+    "revision": "2f4b9cc2437ba537a7e6",
+    "url": "/robotic-rolodex/static/css/main.f4e15f97.chunk.css"
+  },
+  {
+    "revision": "32075dcb124f0dfc859b",
+    "url": "/robotic-rolodex/static/js/2.ef5f4cdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotic-rolodex/static/js/2.ef5f4cdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f4b9cc2437ba537a7e6",
+    "url": "/robotic-rolodex/static/js/main.0ed994f7.chunk.js"
+  },
+  {
+    "revision": "806ab72a80df0b154aad",
+    "url": "/robotic-rolodex/static/js/runtime-main.d45eedb5.js"
+  }
+]);
